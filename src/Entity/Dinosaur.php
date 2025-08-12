3ca5aa2e14a42676559eadf03001a09ed8ee465c
Oamogetswe Mgidi <oamotebogomgidi@gmail.com>
2025-08-12 08:02:08 +0200

    Created a controller class and entity class.

---
+++ src/Entity/Dinosaur.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DinosaurRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=DinosaurRepository::class)
+ */
+class Dinosaur
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=80)
+     */
+    private $genus;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $length;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $enclosure;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getGenus(): ?string
+    {
+        return $this->genus;
+    }
+
+    public function setGenus(string $genus): self
+    {
+        $this->genus = $genus;
+
+        return $this;
+    }
+
+    public function getLength(): ?int
+    {
+        return $this->length;
+    }
+
+    public function setLength(int $length): self
+    {
+        $this->length = $length;
+
+        return $this;
+    }
+
+    public function getEnclosure(): ?string
+    {
+        return $this->enclosure;
+    }
+
+    public function setEnclosure(string $enclosure): self
+    {
+        $this->enclosure = $enclosure;
+
+        return $this;
+    }
+}
